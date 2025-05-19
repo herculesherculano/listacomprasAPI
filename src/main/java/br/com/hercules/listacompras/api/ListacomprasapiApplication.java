adf6165c470ff136c5f85f49befbd50d006a7a0e
Hercules Herculano <hbheroi@gmail.com>
2025-05-19 17:27:38 -0300

    Primeiro commit

---
+++ src/main/java/br/com/hercules/listacompras/api/ListacomprasapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hercules.listacompras.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListacomprasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListacomprasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hercules/listacompras/api/ListacomprasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hercules.listacompras.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListacomprasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
